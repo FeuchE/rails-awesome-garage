30945cded7675b6c2aabd82cf6d3a1fe766090f6
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-15 18:00:17 +0100

    Rails garage - homepage

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root 'pages#index'
+
   resources :cars, only: %i[index show] do
     resources :reviews, only: %i[index show new create]
     resources :favourites, only: %i[new create]
