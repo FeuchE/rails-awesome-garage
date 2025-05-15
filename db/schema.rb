2be982c90223c276a97b91f6b9f32325d5464640
Ellen Feuchtwanger <feuche@gmail.com>
2025-05-15 18:24:48 +0100

    Rails garage - Add images migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_15_081808) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_15_170704) do
   create_table "cars", force: :cascade do |t|
     t.string "brand"
     t.string "model"
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_15_081808) do
     t.integer "owner_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.index ["owner_id"], name: "index_cars_on_owner_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Favourite.destroy_all
+Review.destroy_all
 Car.destroy_all
 Owner.destroy_all
 
@@ -16,8 +18,30 @@ twey = Owner.create(nickname: 'twey')
 
 puts "#{Owner.count} owners created"
 
-Car.create(brand: 'Aston Martin', model: 'DB9', year: 2016, fuel: 'Petrol', owner_id: chloe.id)
-Car.create(brand: 'Jaguar', model: 'XK8', year: 2015, fuel: 'Petrol', owner_id: jack.id)
-Car.create(brand: 'Bugatti', model: 'Veyron', year: 2005, fuel: 'Petrol', owner_id: twey.id)
+Car.create(
+  brand: 'Aston Martin',
+  model: 'DB9',
+  year: 2016,
+  fuel: 'Petrol',
+  image_url: 'https://images.pexels.com/photos/13939446/pexels-photo-13939446.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  owner_id: chloe.id
+)
+
+Car.create(
+  brand: 'Jaguar',
+  model: 'XK8',
+  year: 2015,
+  fuel: 'Petrol',
+  image_url: 'https://goauctionmpc.blob.core.windows.net/stock/2185-1-medium.jpg?v=63846649486840',
+  owner_id: jack.id)
+
+Car.create(
+  brand: 'Bugatti',
+  model: 'Veyron',
+  year: 2005,
+  fuel: 'Petrol',
+  image_url: 'https://news.dupontregistry.com/wp-content/uploads/2016/12/bugatti-veyron-red-black-main.jpg',
+  owner_id: twey.id
+)
 
 puts "#{Car.count} cars created"
